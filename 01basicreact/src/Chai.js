e3998914167835ffb0c94fe7dd569a60452da331
pranavpurankar <pranav.purankar@gmail.com>
2024-09-16 22:06:59 +0530

    react files update

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,9 @@
+
+
+function Chai(){
+    return(
+        <h2>Chai with create-react</h2>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Chai />
+    <h1>Chai aur react with vite | {username}</h1>
+    <p>para text</p>
+    </>
+  )
+}
+
+export default App
