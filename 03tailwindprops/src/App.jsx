583df5f6c55707cbd7e03059234637939325cad0
pranavpurankar <pranav.purankar@gmail.com>
2024-09-27 14:19:03 +0530

    components updated

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,13 +5,19 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let myObj = {
+    username:"pranav",
+    age:28
+  }
+
+
 
   return (
     <>
       <h1 className='bg-green-400 text-black p-5 
       rounded-xl'>Tailwind Test</h1>
-      <Card />
+      <Card username="Pranav Youngest trillionaire" />
       <Card />
     </>
   )
--- 03tailwindprops/src/components/Card.jsx
+++ 03tailwindprops/src/components/Card.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 
-function Card(props){
-    console.log("props", props);
+function Card({username}){
     return (
-        <div class="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
-            <div class="p-4">
-            <h6 class="mb-2 text-slate-800 text-xl font-semibold">
-                Special News Article
+        <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+            <div className="p-4">
+            <h6 className="mb-2 text-slate-800 text-xl font-semibold">
+                {username}
             </h6>
-            <p class="text-slate-600 leading-normal font-light">
+            <p className="text-slate-600 leading-normal font-light">
                 The place is close to Barceloneta Beach and bus stop just 2 min by walk
                 and near to &quot;Naviglio&quot; where you can enjoy the main night life in
                 Barcelona.
             </p>
             </div>
-            <div class="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
+            <div className="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
             <img src="https://images.unsplash.com/photo-1725610588086-b9e38da987f7?q=80&w=1200" alt="card-image" />
             </div>
       </div>
