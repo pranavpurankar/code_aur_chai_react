e45577b1e472a6ef1942c8d71456da1b3b0bfbf4
pranavpurankar <pranav.purankar@gmail.com>
2024-09-28 18:16:20 +0530

    password generator update, copy part is remaining

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
-// import './assets'
+import { useState, useCallback, useEffect } from 'react'
+
 
 function App() {
   const [length, setLength] = useState(8)
@@ -7,35 +7,36 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
-  //useRef hook
-  const passwordRef = useRef(null)
-
-  const passwordGenerator = useCallback(() => {
+  const passwordGenerator = useCallback( () => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if(numberAllowed) str += "0123456789"
-    if(charAllowed) str += "@(~!@#$%^&*_-+=`|\(){}[]:'<>,.?/)"
 
-    for(let i=1; i<= length; i++){
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "?:{}[]@#$%^&*()_+<>,.;'~`"
+
+    for(let i=1; i<=length; i++){
       let char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char)
     }
 
     setPassword(pass)
 
-  }, [length,numberAllowed,charAllowed, setPassword])
+  },
+    [length,numberAllowed, charAllowed, setPassword])
 
   useEffect( () => {
     passwordGenerator()
-  }, [length,numberAllowed,charAllowed,passwordGenerator])
+  },
+  [length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg
-       px-4 py-3 my-8 text-orange-400 bg-gray-700'>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg 
+      px-4 py-3 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-4xl text-center text-white my-3'>
-        Password Generator</h1>
-        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        Password Generator
+        </h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-3'>
           <input 
           type="text" 
           value={password}
@@ -43,48 +44,52 @@ function App() {
           placeholder='Password'
           readOnly
           />
-          <button
-          className='outline-none bg-blue-700 text-white px-3 
-          py-0.5 shrink-0'
-          >copy</button>
-        
+
+          <button className='outline-none bg-blue-700 text-white 
+          px-3 py-0.5 shrink-0'
+          >Copy</button>
+
         </div>
 
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input 
-            type="range"
+            type="range" 
             min={6}
-            max={100}
+            max={30}
             value={length}
             className='cursor-pointer'
             onChange={ (e) => {setLength(e.target.value)}}
             />
-            <label>Length: {length}</label>
+            <label>Length:{length}</label>
           </div>
+          
           <div className='flex items-center gap-x-1'>
-            <input type="checkbox"
-            defaultChecked={numberAllowed}
-            id='numberInput'
-            onChange={() => {
-              setNumberAllowed( (prev) => !prev)
-            }}
+            <input 
+              type="checkbox" 
+              defaultChecked={numberAllowed}
+              id='numberAllowed'
+              onChange={ () => {
+                setNumberAllowed( (prev) => !prev)
+              }}
             />
-            <label htmlFor="numberInput">Numbers</label>
+            <label>Numbers</label>
           </div>
+
           <div className='flex items-center gap-x-1'>
-            <input type="checkbox"
-            defaultChecked={numberAllowed}
-            id='numberInput'
-            onChange={() => {
-              setCharAllowed( (prev) => !prev)
-            }}
+            <input 
+              type="checkbox" 
+              defaultChecked={charAllowed}
+              id='charAllowed'
+              onChange={ () => {
+                setCharAllowed( (prev) => !prev)
+              }}
             />
-            <label htmlFor="numberInput">Characters</label>
+            <label>Characters</label>
           </div>
+        
         </div>
-       
-       </div>
+      </div>
     </>
   )
 }
