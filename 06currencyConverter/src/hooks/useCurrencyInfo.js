986879d5a92a623356e555784dbb14676a0dc25a
pranavpurankar <pranav.purankar@gmail.com>
2024-10-16 20:22:08 +0530

    custom hooks

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -2,20 +2,29 @@ import { useEffect, useState } from "react";
 
 
 function useCurrencyInfo(currency){
-
+    const [data, setData] = useState({})
     useEffect( ()=> {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-    }, [])
+        .then( (res) => res.json())
+        .then( (res) => setData(res[currency]))
+        // console.log(data);        
+    }, [currency])
 
-    // const [data, setData] = useState({})
-    // useEffect ( () => {
-    //     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-    //     .then( (res) => res.json())
-    //     .then( (res) => setData( res[currency]))
-    //     console.log(data);        
-    // }, [currency])
-
-    // return data
+    return data
 }
-
 export default useCurrencyInfo;
+
+
+
+
+
+
+// const [data, setData] = useState({})
+// useEffect ( () => {
+//     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+//     .then( (res) => res.json())
+//     .then( (res) => setData( res[currency]))
+//     console.log(data);        
+// }, [currency])
+
+// return data
