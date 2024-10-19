decbc7eb8703d0c11fcfe19063418643523e161f
pranavpurankar <pranav.purankar@gmail.com>
2024-10-19 14:02:19 +0530

    React router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Github(){
-    const data = useLoaderData()
-    // const [data, setData] = useState([])
-    // useEffect( () => {
+    // const [data, setData]=useState([])
+
+    // useEffect( ()=> {
     //     fetch('https://api.github.com/users/hiteshchoudhary')
-    //     .then( (response => response.json()))
-    //     .then( (data => {
+    //     .then(response => response.json())
+    //     .then(data => {
     //         console.log(data);
     //         setData(data)
-    //     }))
-    // }, [])
+    //     })
+    // },[])
+    const data = useLoaderData()
     return(
         <div className="text-center m-4 bg-gray-500 text-white p-4 text-3xl">Github followers:{data.followers}
         <img src={data.avatar_url} alt="Git picture" width={150}/>
@@ -25,7 +24,5 @@ export default Github
 
 export const githubInfoLoader = async () => {
     const response = await fetch('https://api.github.com/users/hiteshchoudhary')
-
     return response.json()
-
 }
